6127705ca8497423c48c6fa092919cee17f4a6b1
mettmoon <ys_qwerty700@naver.com>
2014-09-18 14:54:37 +0900

    Initial Commit

---
+++ Image Resizer for iOS/IRAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  IRAppDelegate.m
+//  Image Resizer for iOS
+//
+//  Created by Hayden on 2014. 9. 18..
+//  Copyright (c) 2014년 OliveStory. All rights reserved.
+//
+
+#import "IRAppDelegate.h"
+
+@implementation IRAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ Image Resizer for iOSTests/Image_Resizer_for_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  Image_Resizer_for_iOSTests.m
+//  Image Resizer for iOSTests
+//
+//  Created by Hayden on 2014. 9. 18..
+//  Copyright (c) 2014년 OliveStory. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Image_Resizer_for_iOSTests : XCTestCase
+
+@end
+
+@implementation Image_Resizer_for_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
